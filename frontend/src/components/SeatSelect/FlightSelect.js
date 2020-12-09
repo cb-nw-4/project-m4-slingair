7c6259833fc24587ae0dad6d79557ec29828e382
ferielnabet <fnabet@ipconnex.com>
2020-12-09 14:44:42 -0500

    Add dropDOwn flight select

--- frontend/src/components/SeatSelect/FlightSelect.js
+++ frontend/src/components/SeatSelect/FlightSelect.js
@@ -8,15 +8,30 @@ const FlightSelect = ({ handleFlightSelect }) => {
 
   useEffect(() => {
     // TODO: fetch the flight numbers
+    fetch('/flights')
+      .then((res) => res.json())
+      .then((json) => {
+        setFlights(json.data);
+      })
+
   }, []);
 
   return (
     <Wrapper>
       <label htmlFor="flight">Flight Number :</label>
       {/* TODO: Create a dropdown from the flight numbers */}
+
+      <select id='flight' name='flight' onChange={handleFlightSelect}>
+        <option>Select flight</option>
+          {flights.map(flight => 
+            <option key={flight} value={flight}>{flight}</option>
+            
+            )}
+      </select>
     </Wrapper>
   );
 };
+
 
 const Wrapper = styled.div`
   background: ${themeVars.cadmiumRed};

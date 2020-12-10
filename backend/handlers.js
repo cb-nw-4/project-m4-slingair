97223347ece6aa0d3817b49c1d301714cc7322ac
Sandra <sandrabess@hotmail.com>
2020-12-09 19:17:13 -0500

    backend first draft

--- backend/handlers.js
+++ backend/handlers.js
@@ -6,19 +6,176 @@ const { v4: uuidv4 } = require("uuid");
 //  Use this data. Changes will persist until the server (backend) restarts.
 const { flights, reservations } = require("./data");
 
-const getFlights = (req, res) => {};
+const getFlights = (req, res) => {
+  res.status(200).json({ 
+    status: 200,
+    data: Object.keys(flights),
+    message: "Success"
+  }); 
+};
 
-const getFlight = (req, res) => {};
+const getFlight = (req, res) => {
+  const flightData = flights[req.params.flightNum];
+  if (flightData)
+    res.status(200).json({ 
+      status: 200,
+      data: flightData,
+      message: "Success"
+    }); 
+  else
+    res.status(404).json({ 
+      status: 404,
+      data: req.params.flightNum,
+      message: "Flight not found" 
+    });
+};
 
-const addReservations = (req, res) => {};
+const addReservations = (req, res) => {
+  const {    
+    flight,
+    seat,
+    givenName,
+    surname,
+    email
+  } = req.body;
+ 
+  //Peut-etre vrifier si le flight and seat exist dans le flight array et est disponible... ou peut-etre voir si toutes ces valid sont fait frontend]
+  const alreadyTaken = reservations.some((reservation)=>(reservation.flight === flight && reservation.seat === seat));
+  const incomplete = !flight || !seat || !givenName || !surname || !email;  
+  const isEmailValid = /(.+)@(.+){2,}\.(.+){2,}/.test(email);
 
-const getReservations = (req, res) => {};
+  if (alreadyTaken) {
+    res.status(400).json({ 
+      status: 400,
+      data: req.body,
+      message: "Flight and seat already taken"
+    });
+  }
+  else if (incomplete) {
+     res.status(400).json({ 
+      status: 400,
+      data: req.body,
+      message: "Incomplete information"
+    });
+  }
+  else if (!isEmailValid) {
+    res.status(400).json({ 
+      status: 400,
+      data: req.body,      
+      message: "Invalid email"
+    });
+  }
+  else{
+    const newReservation = {id: uuidv4(), ...req.body};   
+    reservations.push(newReservation);
+    res.status(200).json({ 
+      status: 200,
+      data: newReservation,
+      message: "Reservation added"
+    }); 
+  }
 
-const getSingleReservation = (req, res) => {};
+};
 
-const deleteReservation = (req, res) => {};
+const getReservations = (req, res) => {
+  res.status(200).json({ 
+    status: 200,
+    data: reservations,
+    message: "Success"
+  });
 
-const updateReservation = (req, res) => {};
+};
+
+const getSingleReservation = (req, res) => {
+   const theReservation = reservations.find((reservation)=>(reservation.id === req.params.id));
+   if (theReservation)
+    res.status(200).json({ 
+      status: 200,
+      data: theReservation,
+      message: "Success"
+    }); 
+  else
+    res.status(404).json({ 
+      status: 404,
+      data: req.params.id,
+      message: "Reservation not found" 
+    });
+};
+
+const deleteReservation = (req, res) => {
+  const reservationIndex = reservations.findIndex((reservation)=>(reservation.id === req.params.id));
+   if (reservationIndex !== -1){
+    reservations.splice(reservationIndex, 1);
+    res.status(200).json({ 
+      status: 200,
+      data: {},
+      message: "Reservation deleted"
+    }); 
+  }
+  else
+    res.status(404).json({ 
+      status: 404,
+      data: req.params.id,
+      message: "Reservation not found" 
+    });
+};
+
+const updateReservation = (req, res) => {
+  const {  
+    id,  
+    flight,
+    seat,
+    givenName,
+    surname,
+    email
+  } = req.body;
+ 
+  //Peut-etre vrifier si le flight and seat exist dans le flight array et est disponible... ou peut-etre voir si toutes ces valid sont fait frontend] 
+  //Doit-on cr/er un nouveau numero de id?  besoin de verifier id?
+  const reservationIndex = reservations.findIndex((reservation)=>(reservation.id === req.params.id));
+  const isGoodId = id === req.params.id;
+  const incomplete = !id || !flight || !seat || !givenName || !surname || !email;  
+  const isEmailValid = /(.+)@(.+){2,}\.(.+){2,}/.test(email);
+
+  if (reservationIndex === -1) {
+    res.status(400).json({ 
+      status: 400,
+      data: req.body,
+      message: "Reservation not found"
+    });
+  }
+  else if (!isGoodId){
+    res.status(400).json({ 
+      status: 400,
+      data: req.body,
+      message: "Bad id"
+    });
+  }
+  else if (incomplete) {
+     res.status(400).json({ 
+      status: 400,
+      data: req.body,
+      message: "Incomplete information"
+    });
+  }
+  else if (!isEmailValid) {
+    res.status(400).json({ 
+      status: 400,
+      data: req.body,      
+      message: "Invalid email"
+    });
+  }
+  else{
+    const newReservation = {...req.body};   
+    reservations[reservationIndex] = newReservation;
+    res.status(200).json({ 
+      status: 200,
+      data: newReservation,
+      message: "Reservation modified"
+    }); 
+  }
+
+};
 
 module.exports = {
   getFlights,

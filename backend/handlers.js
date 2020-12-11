efc18ea2038fbc94cb95643d2da2e540d0f3de2c
Joni Sadler <joni.anne.sadler@gmail.com>
2020-12-10 20:01:00 -0500

    Endpoints complete

--- backend/handlers.js
+++ backend/handlers.js
@@ -6,19 +6,110 @@ const { v4: uuidv4 } = require("uuid");
 //  Use this data. Changes will persist until the server (backend) restarts.
 const { flights, reservations } = require("./data");
 
-const getFlights = (req, res) => {};
+const getFlights = (req, res) => {
+  res.status(200).json({
+    status: 200,
+    flights: Object.keys(flights),
+  })
+};
 
-const getFlight = (req, res) => {};
+const getFlight = (req, res) => {
+  let flightId = req.params.id;
+  let selectedFlight = flights[flightId];
 
-const addReservations = (req, res) => {};
+  if (selectedFlight) {
+    res.status(200).json({
+      status: 200,
+      seating: selectedFlight,
+    })
+  } else {
+    res.status(404).json({
+      status: 404,
+      seating: "Flight not found.",
+    })
+  }
+};
 
-const getReservations = (req, res) => {};
 
-const getSingleReservation = (req, res) => {};
+const addReservations = (req, res) => {
+  let newReservation = {
+    id: uuidv4(),
+    flight: req.body.flight,
+    seat: req.body.seat,
+    givenName: req.body.givenName,
+    surname: req.body.surname,
+    email: req.body.email,
+  }
 
-const deleteReservation = (req, res) => {};
+  res.status(200).json({
+    status: 200,
+    reservation: newReservation,
+  })
+};
 
-const updateReservation = (req, res) => {};
+
+const getReservations = (req, res) => {
+  res.status(200).json({
+    status: 200,
+    reservations: reservations,
+  })
+};
+
+
+const getSingleReservation = (req, res) => {
+  let reservationId = req.params.id;
+  let selectedReservation = reservations.filter((reservation) => reservationId === reservation.id);
+
+  if (selectedReservation.length > 0 ) {
+    res.status(200).json({
+      status: 200,
+      reservation: selectedReservation,
+    })
+  } else {
+    res.status(404).json({
+      status: 404,
+      reservation: "Reservation not found.",
+    })
+  }
+};
+
+
+const deleteReservation = (req, res) => {
+  let reservationId = req.params.id;
+  let selectedReservation = reservations.filter((reservation) => reservationId === reservation.id);
+
+  if (selectedReservation.length > 0) {
+    res.status(200).json({
+      status: 200,
+      message: "Reservation has been deleted."
+    })
+  } else {
+    res.status(400).json({
+      status: 400,
+      message: "Reservation is not in database.",
+    })
+  }
+};
+
+const updateReservation = (req, res) => {
+  let reservationId = req.params.id;
+  let selectedReservation = reservations.filter((reservation) => reservationId === reservation.id);
+
+  selectedReservation = {
+    id: uuidv4(),
+    flight: req.body.flight,
+    seat: req.body.seat,
+    givenName: req.body.givenName,
+    surname: req.body.surname,
+    email: req.body.email,
+  }
+
+  res.status(200).json({
+    status: 200,
+    reservation: "Reservation has been updated.",
+  })
+
+};
 
 module.exports = {
   getFlights,
--- frontend/src/components/SeatSelect/FlightSelect.js
+++ frontend/src/components/SeatSelect/FlightSelect.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import Plane from "./Plane";
 
 import { themeVars } from "../GlobalStyles";
 
@@ -10,10 +11,31 @@ const FlightSelect = ({ handleFlightSelect }) => {
     // TODO: fetch the flight numbers
   }, []);
 
+  let [showMenu, setShowMenu] = useState(false);
+
   return (
     <Wrapper>
       <label htmlFor="flight">Flight Number :</label>
-      {/* TODO: Create a dropdown from the flight numbers */}
+
+      <div>
+        <Button onClick={() => setShowMenu(showMenu = true)}>
+          <FlightSelection>Select a flight ▼</FlightSelection>
+        </Button>
+
+        { showMenu === true
+          ? (
+            <DropdownContainer>
+              <Flight>Select a flight</Flight>
+              <Flight onClick={() => FlightSelect()}>SA231</Flight>
+            </DropdownContainer>
+          )
+          : (
+            null
+          )
+        }
+        
+      </div>
+
     </Wrapper>
   );
 };
@@ -26,5 +48,37 @@ const Wrapper = styled.div`
   padding: ${themeVars.pagePadding};
   margin-bottom: ${themeVars.pagePadding};
 `;
+ 
+const Button = styled.button` 
+  margin-left: 20px;
+  border-radius: 3px;
+  border: none;
+  background-color: white;
+  width: 130px;
+`;
+
+const FlightSelection = styled.p` 
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 14px;
+  color: black;
+  padding: 5px;
+`
+
+const DropdownContainer = styled.div` 
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  margin-left: 20px;
+  border: 1px solid black;
+  position: absolute;
+  width: 130px;
+`;
+
+const Flight = styled.a`
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 14px;
+  padding: 5px;
+`;
+
 
 export default FlightSelect;

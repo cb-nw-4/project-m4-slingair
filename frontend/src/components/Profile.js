641bbf296f42f761c309682dd7feeab6c36e8ded
chris shino <chris.shino@hotmail.com>
2020-12-15 19:11:41 -0500

    added a disabled function to my onclick in profile

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -7,17 +7,21 @@ import SeatSelect from "./SeatSelect";
 
 const Profile = ({ updateUserReservation, userReservation }) => {
   const [isDeleting, setIsDeleting] = useState(false)
+  const [isLongerThanZero, setIsLongerThanZero] = useState(true)
 
   const handleDelete = () => {
     fetch(`/reservation/${userReservation.id}`, {
       method: "DELETE",
     });
     setIsDeleting(true)
+    fetch('/reservation')
+    .then(res => res.json())
+    .then(json => json.body.length > 1 ? setIsLongerThanZero(true) : setIsLongerThanZero(false))
   };
 
 
   return (
-    <>
+    <Wrapper>
       <SeatSelect
         isDeleting={isDeleting}
         setIsDeleting={setIsDeleting}
@@ -25,9 +29,21 @@ const Profile = ({ updateUserReservation, userReservation }) => {
         updateUserReservation={updateUserReservation}
         userReservation={userReservation}
       />
-      <button onClick={handleDelete}>Delete</button>
-    </>
+      <MyButton onClick={handleDelete} disabled={!isLongerThanZero}>Delete</MyButton>
+    </Wrapper>
   );
 };
 
 export default Profile;
+
+const Wrapper = styled.div`
+  
+`;
+
+const MyButton = styled.button`
+  margin-top: 1rem;
+  margin-left: 25%;
+  margin-right: 25%;
+  padding: 1rem 4rem;
+  width: 50%;
+`;
